be0b213330cf1ffd21259fa830923b913fd3e6bb
Mr. Saquib Aowte <56401349+SaquibAowte@users.noreply.github.com>
2024-11-25 15:29:53 +0530

    Changes in App.js

--- src/App.js
+++ src/App.js
@@ -18,22 +18,41 @@ import "./App.css"
 const App = () => {
     return (
         <>
-            <MyNav />
-            <Routes>
-                <Route path="/" index element={<Home />} />
-                <Route path="/About-us" element={<About />} />
-                <Route path="/Activities" element={<Activities />} />
-                <Route path="/nearest-attraction" element={<NearestAttraction />} />
-                <Route path="/restaurant" element={<Restaurant />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/gallery" element={<Gallery />} />
-                <Route path="/testimonials" element={<Testimonials />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/all_blog" element={<AllBlogs />} />
-                <Route path="/luxuryTents" element={<Tents />} />
-                <Route path="/cottages" element={<Cottages />} />
-            </Routes>
-            <Footer />
+            // <MyNav />
+            // <Routes>
+            //     <Route path="/" index element={<Home />} />
+            //     <Route path="/About-us" element={<About />} />
+            //     <Route path="/Activities" element={<Activities />} />
+            //     <Route path="/nearest-attraction" element={<NearestAttraction />} />
+            //     <Route path="/restaurant" element={<Restaurant />} />
+            //     <Route path="/contact" element={<Contact />} />
+            //     <Route path="/gallery" element={<Gallery />} />
+            //     <Route path="/testimonials" element={<Testimonials />} />
+            //     <Route path="/blog" element={<Blog />} />
+            //     <Route path="/all_blog" element={<AllBlogs />} />
+            //     <Route path="/luxuryTents" element={<Tents />} />
+            //     <Route path="/cottages" element={<Cottages />} />
+            // </Routes>
+            // <Footer />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+                backgroundColor: "#f9f9f9",
+                fontFamily: "Arial, sans-serif",
+                textAlign: "center",
+                color: "#ff4d4f",
+              }}
+            >
+              <div>
+                <h1 style={{ fontSize: "5rem", margin: 0 }}>403</h1>
+                <p style={{ fontSize: "1.5rem", margin: "10px 0" }}>
+                  403: Access Denied: Unable to fetch data.
+                </p>
+              </div>
+            </div>
         </>
 
     );
