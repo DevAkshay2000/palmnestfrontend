f5b5d10c89f5614f2cf85c2db3f3689a9a245dae
Mr. Saquib Aowte <56401349+SaquibAowte@users.noreply.github.com>
2024-11-25 15:38:27 +0530

    new comment done in app file

--- src/App.js
+++ src/App.js
@@ -18,22 +18,22 @@
 const App = () => {
     return (
         <>
-            // <MyNav />
-            // <Routes>
-            //     <Route path="/" index element={<Home />} />
-            //     <Route path="/About-us" element={<About />} />
-            //     <Route path="/Activities" element={<Activities />} />
-            //     <Route path="/nearest-attraction" element={<NearestAttraction />} />
-            //     <Route path="/restaurant" element={<Restaurant />} />
-            //     <Route path="/contact" element={<Contact />} />
-            //     <Route path="/gallery" element={<Gallery />} />
-            //     <Route path="/testimonials" element={<Testimonials />} />
-            //     <Route path="/blog" element={<Blog />} />
-            //     <Route path="/all_blog" element={<AllBlogs />} />
-            //     <Route path="/luxuryTents" element={<Tents />} />
-            //     <Route path="/cottages" element={<Cottages />} />
-            // </Routes>
-            // <Footer />
+        {/*<MyNav />
+            <Routes>
+                <Route path="/" index element={<Home />} />
+                <Route path="/About-us" element={<About />} />
+                <Route path="/Activities" element={<Activities />} />
+                <Route path="/nearest-attraction" element={<NearestAttraction />} />
+                <Route path="/restaurant" element={<Restaurant />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="/testimonials" element={<Testimonials />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/all_blog" element={<AllBlogs />} />
+                <Route path="/luxuryTents" element={<Tents />} />
+                <Route path="/cottages" element={<Cottages />} />
+            </Routes>
+            <Footer />*/}
             <div
               style={{
                 display: "flex",
