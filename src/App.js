a14b01a826b3cc73f4fae76f521e46880c0ee348
Mr. Saquib Aowte <56401349+SaquibAowte@users.noreply.github.com>
2025-05-02 17:39:39 +0530

    Uncommented App.js code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-// import { Routes, Route } from "react-router-dom";
-// import Home from "./components/Home";
-// import About from "./components/About";
-// import MyNav from "./components/MyNav";
-// import Footer from "./components/Footer";
-// import Activities from "./components/Activities";
-// import NearestAttraction from "./components/NearestAttraction";
-// import Restaurant from "./components/Restaurant";
-// import Contact from "./components/Contact";
-// import Gallery from "./components/Resources/Gallery";
-// import ImageGallery from "./components/Resources/ImageGallery";
-// import Testimonials from "./components/Resources/Testimonials";
-// import Blog from "./components/Resources/Blog";
-// import AllBlogs from "./components/Resources/AllBlog";
-// import Tents from "./components/Accomodation/Tents";
-// import Cottages from "./components/Accomodation/Cottages";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import MyNav from "./components/MyNav";
+import Footer from "./components/Footer";
+import Activities from "./components/Activities";
+import NearestAttraction from "./components/NearestAttraction";
+import Restaurant from "./components/Restaurant";
+import Contact from "./components/Contact";
+import Gallery from "./components/Resources/Gallery";
+import ImageGallery from "./components/Resources/ImageGallery";
+import Testimonials from "./components/Resources/Testimonials";
+import Blog from "./components/Resources/Blog";
+import AllBlogs from "./components/Resources/AllBlog";
+import Tents from "./components/Accomodation/Tents";
+import Cottages from "./components/Accomodation/Cottages";
 import './App.css';
 const App = () => {
   return (
     <>
-      {/* <MyNav />
+      <MyNav />
       <Routes>
         <Route path="/" index element={<Home />} />
         <Route path="/About-us" element={<About />} />
@@ -34,11 +34,10 @@ const App = () => {
         <Route path="/luxuryTents" element={<Tents />} />
         <Route path="/cottages" element={<Cottages />} />
       </Routes>
-      <Footer /> */}
-      <h3 className="text-center">
-        {' '}
+      <Footer />
+      {/* <h3 className="text-center">
         <span color="red">Error 404: </span> Not Found!{' '}
-      </h3>
+      </h3> */}
     </>
   );
 };
